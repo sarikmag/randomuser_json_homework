1d9da0adf80a9c84b9006f14b90b936b3fecdefd
ZARIFMAC <uzbzarif@gmail.com>
2022-11-23 09:42:11 +0500

    count users

---
+++ count_users.py
@@ -0,0 +1,16 @@
+from from_json import read_json
+
+def count_users(data:dict)->int:
+    """Counts the number of users in the data
+
+    Args:
+        data (dict): The data from the JSON file
+
+    Returns:
+        int: The number of users
+    """
+    # Count the number of users
+    return 0
+
+data = read_json("users.json")
+print(count_users(data))
